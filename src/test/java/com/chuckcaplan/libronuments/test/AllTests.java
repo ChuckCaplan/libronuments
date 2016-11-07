74abbb1af86be8130304184abec93427dc6049fa
Chuck Caplan <chuck@Admins-MacBook-Air-2.local>
2016-11-07 13:54:04 -0500

    added controller unit tests

--- src/test/java/com/chuckcaplan/libronuments/test/AllTests.java
+++ src/test/java/com/chuckcaplan/libronuments/test/AllTests.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.chuckcaplan.libronuments.test.controller.AllControllerTests;
 import com.chuckcaplan.libronuments.test.dao.AllDAOTests;
 import com.chuckcaplan.libronuments.test.entity.AllEntityTests;
 import com.chuckcaplan.libronuments.test.service.AllServiceTests;
@@ -15,7 +16,7 @@ import com.chuckcaplan.libronuments.test.service.AllServiceTests;
  *
  */
 @RunWith(Suite.class)
-@SuiteClasses({ AllDAOTests.class, AllEntityTests.class, AllServiceTests.class })
+@SuiteClasses({ AllDAOTests.class, AllEntityTests.class, AllServiceTests.class, AllControllerTests.class })
 public class AllTests {
 
 }
